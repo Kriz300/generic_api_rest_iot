397377ad0960d8d99a60c560ca638d00bebe6c24
Kriz300 <christian.munoz1@mail.udp.cl>
2022-12-05 17:58:15 -0300

    fix

--- utils/db_init.py
+++ utils/db_init.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('Backbone.db')
+conn = sqlite3.connect('../Backbone.db')
 cur = conn.cursor()
 
 #Crea las tablas
@@ -10,16 +10,16 @@ with open('schemes.sql') as f:
 '''
 
 #Crea al Admin
-cur.execute("INSERT INTO admin VALUES ('admin','pass')")
+cur.execute("INSERT INTO Admin VALUES ('admin','pass')")
 
 #Crea Company
-cur.execute("INSERT INTO company VALUES (1,'prime','c_ssak_1')")
+cur.execute("INSERT INTO Company VALUES (1,'prime','c_ssak_1')")
 
 #Crea ubicaciones
-cur.execute("INSERT INTO location VALUES (1,'local1','Chile','stgo','Robada 3 veces')")
+cur.execute("INSERT INTO Location VALUES (1,'local1','Chile','stgo','Robada 3 veces')")
 
 #Crea sensores
-cur.execute("INSERT INTO sensor VALUES (1,1,'sensor1','movimiento','Si te mueves te ve','s_ssak_1')")
+cur.execute("INSERT INTO Sensor VALUES (1,1,'sensor1','movimiento','Si te mueves te ve','s_ssak_1')")
 
 conn.commit()
 conn.close()
